5614c57e9f1c41042e98bada99a66bef681dac12
Ruth Stiebel <stiebelruth@gmail.com>
2025-06-29 02:49:27 +0300

    Removed more unused imports modified:   inklet-frontend/src/Pages/Canvas.tsx

--- inklet-frontend/src/Pages/Canvas.tsx
+++ inklet-frontend/src/Pages/Canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import DrawingCanvas, {
   type DrawingCanvasRef,
 } from "../components/canvas/DrawingCanvas";
